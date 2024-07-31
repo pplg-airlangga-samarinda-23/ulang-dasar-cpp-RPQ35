efc6f58d81d6bc6efe510c514a264874a29a9d57
ropiq <alfajriropiq@gmail.com>
2024-07-31 12:22:33 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
+
+void cetakdata(string a,string b,string c,string d){
+    //cetak data nama,nisn,jurusan,dan timggi badan
+    cout<<"nama          = "<<a<<endl;
+    cout<<"nisn          = "<<b<<endl;
+    cout<<"jurusan       = "<<c<<endl;
+    cout<<"tinggi badan  = "<<d<<endl;
+}
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama="ropiq",nisn="0132455623435523",jurusan="pplg",tinggib="165 ";
+   
+//buat variable untuk menyimpan 
+
+
+//panghgil func
+   cetakdata(nama,nisn,jurusan,tinggib);
+
     return 0;
 }
